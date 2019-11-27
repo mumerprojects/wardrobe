6c3f64c37b5f2e72620ae63bfa97389c905533f9
Muhammad Umer <mumeroffice@gmail.com>
2019-11-27 17:55:33 +0500

    change db

--- config/database.php
+++ config/database.php
@@ -52,12 +52,16 @@ return [
             'password' => env('DB_PASSWORD', 'Master#3key'),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
+            'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
             'prefix' => '',
+            'prefix_indexes' => true,
             'strict' => true,
             'engine' => null,
+            'options' => extension_loaded('pdo_mysql') ? array_filter([
+                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+            ]) : [],
         ],
-
         'pgsql' => [
             'driver' => 'pgsql',
             'url' => env('DATABASE_URL'),
